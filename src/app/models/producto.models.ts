8cb03d0c453f71435ebf065c5908ea8b076df8a1
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-07 23:24:18 -0500

    Creacion del modelo producto y componentes pricipales

---
+++ src/app/models/producto.models.ts
@@ -0,0 +1,13 @@
+export class Producto {
+
+    nombre: string;
+    imgUrl: string;
+    precio: number;
+
+    constructor(pNombre: string, pImgUrl: string, pPrecio: number) {
+        this.nombre = pNombre;
+        this.imgUrl = pImgUrl;
+        this.precio = pPrecio;
+    }
+
+}
