fb21e5b5b6e4ec881f58de18d336220a82f4c087
Cristian Gutierrez <cdgutierrez456@gmail.com>
2021-06-08 13:28:00 -0500

    Estilizacion base, creacion de arreglos principales y directrices Input principales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Producto } from './models/producto.models'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CajaRegistradora';
+  
+  arrComida: Producto[];
+  arrBebida: Producto[];
+
+  constructor() {
+    this.arrComida = [
+      new Producto('Bayas', 'https://cdn.pixabay.com/photo/2010/12/13/10/05/berries-2277_960_720.jpg', 5000),
+      new Producto('Fresas', 'https://cdn.pixabay.com/photo/2018/04/29/11/54/strawberries-3359755_960_720.jpg', 3500),
+      new Producto('Hamburguesa', 'https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_960_720.jpg', 15000),
+      new Producto('Pizza', 'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_960_720.jpg', 20000),
+      new Producto('Sushi', 'https://cdn.pixabay.com/photo/2017/10/15/11/41/sushi-2853382_960_720.jpg', 15000)
+    ]
+    this.arrBebida = [
+      new Producto('Coca Cola', 'https://cdn.pixabay.com/photo/2015/06/25/16/56/coca-cola-821512_960_720.jpg', 3000),
+      new Producto('Café', 'https://cdn.pixabay.com/photo/2015/03/26/09/40/coffee-690054_960_720.jpg', 3000),
+      new Producto('Cerveza', 'https://cdn.pixabay.com/photo/2015/09/24/20/36/beer-barrel-956322_960_720.jpg', 23000),
+      new Producto('Cockteles', 'https://cdn.pixabay.com/photo/2016/11/19/13/53/margarita-1839361_960_720.jpg', 30000)
+    ]
+  }
+
 }
--- src/app/components/lista-productos/lista-productos.component.ts
+++ src/app/components/lista-productos/lista-productos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Producto } from 'src/app/models/producto.models';
 
 @Component({
   selector: 'app-lista-productos',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-productos.component.css']
 })
 export class ListaProductosComponent implements OnInit {
+
+  @Input() titulo: string;
+  @Input() productos: Producto[];
 
   constructor() { }
 
